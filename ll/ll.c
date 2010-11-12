3c44eb90201bb740c1c5fb9cade614c68def4515
Pablo Alejandro Costesich <rhfixer@gmail.com>
2010-11-12 02:17:34 -0300

    Added doubly linked list support

---
+++ ll/ll.c
@@ -0,0 +1,146 @@
+/*
+ *      ll.c
+ *
+ *      Copyright 2010:
+ *          Pablo Alejandro Costesich <pcostesi@alu.itba.edu.ar>
+ *
+ *      Redistribution and use in source and binary forms, with or without
+ *      modification, are permitted provided that the following conditions are
+ *      met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following disclaimer
+ *        in the documentation and/or other materials provided with the
+ *        distribution.
+ *      * Neither the name of the Owner nor the names of its
+ *        contributors may be used to endorse or promote products derived from
+ *        this software without specific prior written permission.
+ *
+ *      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *      "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *      LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *      A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *      OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *      SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *      LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *      DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *      THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *      (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *      OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "ll.h"
+
+struct List{
+    size_t size;
+    void * val;
+    struct List * next;
+    struct List * prev;
+};
+
+llist * ll_new(void * val, size_t size){
+    llist * list = malloc(sizeof(llist));
+    if (list == NULL)
+        return NULL;
+    size = size == 0 ? strlen(val) + 1 : size;
+    list->val = malloc(size);
+    if (list->val == NULL){
+        free(list);
+        return NULL;
+    }
+    list->size = size;
+    memcpy(list->val, val, size);
+    list->next = NULL;
+    list->prev = NULL;
+    return list;
+}
+
+llist * ll_append(llist * list, void * val, size_t size){
+    return ll_insert(ll_tail(list), val, size);
+}
+
+llist * ll_remove(llist * list){
+    llist * prev = list->prev;
+    llist * next = list->next;
+    if (prev)
+        prev->next = next;
+    if (next)
+        next->prev = prev;
+    list->prev = NULL;
+    list->next = NULL;
+    return prev ? prev : next;
+}
+
+llist * ll_tail(llist * list){
+    if (list != NULL)
+        while(list->next)
+            list = list->next;
+    return list;
+}
+
+llist * ll_head(llist * list){
+    if (list != NULL)
+        while(list->prev)
+            list = list->prev;
+    return list;
+}
+
+llist * ll_paste(llist * list, llist * n){
+    llist * tail = ll_tail(n);
+    if (tail)
+        tail->next = list->next;
+    if (list->next)
+        list->next->prev = tail;
+    n->prev = list;
+    list->next = n;
+    return n;
+}
+
+llist * ll_update(llist * list, void * val, size_t size){
+    void * aux = malloc(size);
+    if (aux == NULL)
+        return NULL;
+    free(list->val);
+    list->val = aux;
+    memcpy(aux, val, size);
+    list->size = size;
+    return list;
+}
+
+llist * ll_insert(llist * list, void * val, size_t size){
+    llist * n = ll_new(val, size);
+    if (n == NULL)
+        return NULL;
+    return list != NULL ? ll_paste(list, n): n;
+}
+
+void ll_free(llist * list){
+    llist * aux;
+    for(; list != NULL; list = aux){
+        aux = list->next;
+        free(list->val);
+        free(list);
+    }
+}
+
+size_t ll_get(llist * list, void * buffer, size_t size){
+    /* get at most size bytes, unless size is zero (then we default to
+     * list->size. */
+    if (list == NULL || buffer == NULL)
+        return 0;
+    size = size == 0 || list->size < size ? list->size : size;
+    memcpy(buffer, list->val, size);
+    return size;
+}
+
+llist * ll_next(llist * list){
+    return list->next;
+}
+
+llist * ll_prev(llist * list){
+    return list->prev;
+}
---
+++ ll/main.c
@@ -0,0 +1,66 @@
+/*
+//      main.c
+//
+//      Copyright 2010 Pablo Alejandro Costesich <pcostesi@alu.itba.edu.ar>
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ll.h"
+
+#define BUFFER_SIZE 64
+
+void print_haiku(llist * list){
+    char buffer[BUFFER_SIZE];
+    printf("*~~( Haiku )~~~~~~~*\n");
+    printf("~                  ~\n");
+    for(; list != NULL; list = ll_next(list)){
+        ll_get(list, buffer, BUFFER_SIZE);
+        printf("~ %-16s ~\n", buffer);
+    }
+    printf("~                  ~\n");
+    printf("*~~~~~~~~~~~~~~~~~~*\n");
+}
+
+int main(int argc, char **argv)
+{
+    llist * list = NULL;
+    llist * list2 = NULL;
+    char *haiku[] = {"this is unix foo",
+                     "ll_get the data",
+                     "buffer explodes."};
+    list = ll_insert(list, haiku[0], strlen(haiku[0]) + 1);
+    ll_append(list, haiku[1], strlen(haiku[1]) + 1);
+    ll_append(list, haiku[2], strlen(haiku[2]) + 1);
+
+    print_haiku(list);
+
+    ll_update(ll_prev(ll_tail(list)), "ll_update tests", \
+            strlen("ll_update tests") + 1);
+
+    print_haiku(list);
+
+    list2 = list;
+    list = ll_remove(list);
+    print_haiku(list);
+
+    free(list2);
+    ll_free(list);
+    return 0;
+}
